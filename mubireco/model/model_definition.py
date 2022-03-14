b6a750b814ff9070fd20242b9d1f29ec5609e953
damian-taranto <damian.taranto@vodafone.com>
2022-03-14 08:22:25 +0000

    GRU modelling

--- mubireco/model/model_definition.py
+++ mubireco/model/model_definition.py
@@ -9,44 +9,19 @@ from mubireco.data.train import TrainDataset
 class CandidateEncoder(tf.keras.layers.Layer):
     def __init__(self, unique_movie_ids, embedding_dimension):
         super().__init__()
-        self._embedding_model = tf.keras.Sequential([
+        self._embedding_movie_ids = tf.keras.Sequential([
             tf.keras.layers.IntegerLookup(vocabulary=unique_movie_ids, mask_token=None),
             tf.keras.layers.Embedding(len(unique_movie_ids) + 1, embedding_dimension),
         ])
 
-        self._norm_model_years = tf.keras.layers.LayerNormalization(axis=-1)
-        self._norm_model_scores = tf.keras.layers.LayerNormalization(axis=-1)
-
-        self._feature_columns = {
-            "user_eligible_for_trial": tf.feature_column.numeric_column("user_eligible_for_trial"),
-            "user_has_payment_method": tf.feature_column.numeric_column("user_has_payment_method"),
-            "user_subscriber": tf.feature_column.numeric_column("user_subscriber"),
-            "user_trialist": tf.feature_column.numeric_column("user_trialist"),
-        }
-
         self._gru_encoder = tf.keras.layers.GRU(units=embedding_dimension, recurrent_initializer="glorot_uniform")
 
-        self._dense_model = tf.keras.Sequential([
-            tf.keras.layers.DenseFeatures(self._feature_columns.values()),
-            tf.keras.layers.LayerNormalization(axis=1),
-            tf.keras.layers.Dense(4, activation="relu")
-        ])
-
-        self._proj_layer = tf.keras.layers.Dense(embedding_dimension, activation="relu")
-
     def call(self, features):
-        seq_embedding = self._embedding_model(features["previous_movie_ids"])
-        seq_scores = tf.expand_dims(self._norm_model_scores(features["previous_score"]), -1)
+        seq_embedding_movie_ids = self._embedding_movie_ids(features["previous_movie_ids"])
 
-        concat_sequence = tf.concat([seq_embedding, seq_scores], axis=-1)
+        encoder = self._gru_encoder(seq_embedding_movie_ids)
 
-        encoder = self._gru_encoder(concat_sequence)
-
-        concat_encoder = tf.concat([encoder, self._dense_model(features)], axis=-1)
-
-        proj_layer = self._proj_layer(concat_encoder)
-
-        return proj_layer
+        return encoder
 
 
 class MubiMoviesModel(tfrs.Model):
@@ -72,7 +47,6 @@ def create_model(batch_size, embedding_dimension, config):
     df_movies = MoviesDataset(config).read_tf_dataset()
     ds_train = TrainDataset(config).read_tf_dataset()
     unique_movie_ids = np.unique(np.concatenate(list(df_movies.batch(batch_size).map(lambda x: x["movie_id"]))))
-    unique_user_ids = np.unique(np.concatenate(list(ds_train.batch(batch_size).map(lambda x: x["user_id"]))))
 
     query_model = tf.keras.Sequential([
         tf.keras.layers.IntegerLookup(vocabulary=unique_movie_ids, mask_token=None),
